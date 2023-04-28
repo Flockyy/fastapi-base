09558c37f6168db3ca26e86d4b2970f79298ed39
Flockyy <florian.abgrall750@gmail.com>
2023-04-28 16:18:48 +0200

    ''

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,7 +3,7 @@ from typing import Generator
 import pytest
 from fastapi.testclient import TestClient
 
-from db.session import SessionLocal
+from src.db.session import SessionLocal
 from main import app
 
 @pytest.fixture(scope="session")
